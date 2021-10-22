d9536bc027fc1a2580914138cf83a0a06ca89fed
Darshan110801 <73900965+Darshan110801@users.noreply.github.com>
2021-10-22 16:16:50 +0530

    Add files via upload

---
+++ static/questions.js
@@ -0,0 +1,70 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What new element was discovered on the Moon?",
+    answer: "Armalcolite",
+    options: [
+      "Ferrite",
+      "Selenium",
+      "Armalcolite",
+      "Malcoarmite"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does ISRO stand for?",
+    answer: " Indian Space Research Organisation",
+    options: [
+      " Indiana Space Requisite Organisation",
+      " Indian Station Research Organisation",
+      " Indian Scholar Research Organisation",
+      " Indian Space Research Organisation"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which was the first satellite launched by ISRO?",
+    answer: "Kalpana-1",
+    options: [
+      "Kalpana-1",
+      "Bhaskara",
+      "Aryabhata",
+      "Chandrayaan"
+    ]
+  },
+    {
+    numb: 4,
+    question: "In which place Dr Vikram Sarabhai Space Centre is located?",
+    answer: "Sriharikota",
+    options: [
+      "Thiruvananthapuram",
+      "Bangalore",
+      "Sriharikota",
+      "None Of these"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Who is the father of modern space science in India?",
+    answer: "Vikram Sarabhai",
+    options: [
+      "Satish Dhawan",
+      "Vikram Sarabhai",
+      "Dr. Apj abdul kalam",
+      "C V Raman"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
